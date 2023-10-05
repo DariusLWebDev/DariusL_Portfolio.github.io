e59d630071a25fbe94652cf635a412be7ce6601e
DariusLWebDev <143473187+DariusLWebDev@users.noreply.github.com>
2023-10-05 23:25:36 +0800

    Set up new Portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings: ["Soon to be Front End Developer!","Soon to be Back End Developer!", "Soon to be Full Stack Web Developer!"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
